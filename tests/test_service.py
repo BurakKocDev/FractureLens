7b85156544bd709379b5a38119d100c7bf7ca4b4
burak Koc <22burakkoc@gmail.com>
2026-09-13 02:10:49 +0300

    fix: redirect file-opened UI to local API

--- tests/test_service.py
+++ tests/test_service.py
@@ -14,6 +14,7 @@ def test_web_app_is_available_without_loading_models() -> None:
     assert 'id="image-input"' in response.text
     assert '"fracture_signal_with_local_evidence"' in response.text
     assert '"no_fracture_signal"' in response.text
+    assert 'window.location.protocol === "file:"' in response.text
 
 
 def test_health_does_not_load_models() -> None:
